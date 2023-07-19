d8b4f9ab959e26b5c68cd9ffd431a6d48bdedf22
hunterlan <konstantin.sharykin@outlook.com>
2023-07-19 22:32:58 +0200

    Add project files.

---
+++ Login/Pages/Models/EngagmentForm.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Login.Pages.Models
+{
+    public class EngagmentForm
+    {
+        [Required]
+        public string Altweet { get; set; } = null!;
+    }
+}
