be8109711ecec4d121f95f5b6f733520e04262d3
hunterlan <konstantin.sharykin@outlook.com>
2023-07-19 22:50:39 +0200

    Add login and signup pages

---
+++ Login/Pages/Models/LoginForm.cs
@@ -0,0 +1,9 @@
+﻿namespace Login.Pages.Models
+{
+    public class LoginForm
+    {
+        public string Nickname { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Login/Pages/Models/SignupForm.cs
@@ -0,0 +1,11 @@
+﻿namespace Login.Pages.Models
+{
+    public class SignupForm
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Nickname { get; set; } = null!;
+    }
+}
